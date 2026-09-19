c55cf695b1165994e3ae42747eb39aaa2c327c01
SergiiMost <47760161+SergiiMost@users.noreply.github.com>
2026-09-19 11:45:01 -0400

    fix(js-lib2): udpate signature (#13)

--- libs/js-lib2/src/lib/js-lib2.spec.ts
+++ libs/js-lib2/src/lib/js-lib2.spec.ts
@@ -2,6 +2,6 @@ import { jsLib } from './js-lib2';
 
 describe('jsLib', () => {
   it('should work', () => {
-    expect(jsLib()).toEqual('js-lib2-here');
+    expect(jsLib()).toEqual('js-lib2-here!');
   });
 });
